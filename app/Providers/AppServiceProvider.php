3b0d7fd428000015c11007cc80ffe1df12247055
tomislav.papic <tomislavpapic7@gmail.com>
2019-03-13 22:06:42 +0100

    Fix config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Stripe\Stripe;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Stripe::setApiKey(config('services.stripe.secret'));
+
     }
 
     /**
