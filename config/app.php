0906a7c175f3f10242b848c8aef15625fbf8546b
tomislav.papic <tomislavpapic7@gmail.com>
2019-04-02 17:38:20 +0200

    Add aop but not work

--- config/app.php
+++ config/app.php
@@ -177,7 +177,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class
+        App\Providers\RouteServiceProvider::class,
+        App\Providers\AopServiceProvider::class
     ],
 
     /*
